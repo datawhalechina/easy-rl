1e60b688fcb8586eff75c07324182571feb935bb
johnjim0816 <johnjim0816@gmail.com>
2021-09-20 15:33:12 +0800

    update Q-learning

--- codes/common/plot.py
+++ codes/common/plot.py
@@ -5,7 +5,7 @@ Author: John
 Email: johnjim0816@gmail.com
 Date: 2020-10-07 20:57:11
 LastEditor: John
-LastEditTime: 2021-09-15 14:56:15
+LastEditTime: 2021-09-19 23:00:36
 Discription: 
 Environment: 
 '''
@@ -29,6 +29,7 @@ def plot_rewards_cn(rewards,ma_rewards,tag="train",env='CartPole-v0',algo = "DQN
     ''' 中文画图
     '''
     sns.set()
+    plt.figure()
     plt.title(u"{}环境下{}算法的学习曲线".format(env,algo),fontproperties=chinese_font())
     plt.xlabel(u'回合数',fontproperties=chinese_font())
     plt.plot(rewards)
@@ -36,7 +37,7 @@ def plot_rewards_cn(rewards,ma_rewards,tag="train",env='CartPole-v0',algo = "DQN
     plt.legend((u'奖励',u'滑动平均奖励',),loc="best",prop=chinese_font())
     if save:
         plt.savefig(path+f"{tag}_rewards_curve_cn")
-    plt.show()
+    # plt.show()
 
 def plot_losses(losses,algo = "DQN",save=True,path='./'):
     sns.set()
